4004a90583a387530707dba266fdc9bc94fe8fd2
NUÑEZ <luis.nunez.p@uni.pe>
2024-11-10 23:15:06 -0500

    2

--- ProyectoColegio/src/main/java/pe/edu/uni/ProyectoColegio/controller/Servicio1Controller.java
+++
@@ -1,8 +0,0 @@
-package pe.edu.uni.ProyectoColegio.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class Servicio1Controller {
-
-}
--- ProyectoColegio/src/main/java/pe/edu/uni/ProyectoColegio/service/Servicio1.java
+++
@@ -1,5 +0,0 @@
-package pe.edu.uni.ProyectoColegio.service;
-
-public class Servicio1 {
-
-}
