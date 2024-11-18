ae63c30b8272da7c77b1730d1612f4e805175f3e
NUÑEZ <luis.nunez.p@uni.pe>
2024-11-18 00:14:01 -0500

    ajustes de pago

--- Cargar Datos.sql
+++ Cargar Datos.sql
@@ -131,7 +131,7 @@ INSERT INTO MATRICULA (alu_id, sec_id, emp_id, mat_tipo, mat_fecha, mat_estado)
 VALUES 
     (1, 1, 1, 'REGULAR', '2023-03-01', 1),
     (2, 2, 1, 'REGULAR', '2023-03-01', 1),
-    (3, 3, 1, 'REGULAR', '2023-03-01', 1);
+    (3, 3, 1, 'REGULAR', '2023-03-01', 0);
 GO
 
 UPDATE SECCION
